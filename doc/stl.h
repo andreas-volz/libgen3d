da0f9bfd267dac19528a2c3063c7973302bb4611
Andreas Volz <andreas-volz@users.noreply.github.com>
2009-02-12 12:20:25 +0000

    split generic parts of animorph to libgen3d, because I it is useable for other apps too

---
+++ doc/stl.h
@@ -0,0 +1,8 @@
+// don't include this file!!!
+// it's only a dummy header for doxygen documentation
+namespace std {
+  template<class T> class vector { public: T element; };
+  template<class T> class list { public: T element; };
+  template<class T, class T2> class map { public: T element; T2 element};
+  template<class T, class T2> class hash_map { public: T element; T2 element};
+}
